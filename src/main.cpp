af680c8059545ac08011b2ad0d7c36bdf9bb71ff
pety02 <pety02@abv.bg>
2026-06-16 22:37:50 +0300

    Main method implemented to use the command line interpreter implemented in the app.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,64 @@
 #include <iostream>
-#include "Map.h"
 
-int main() {
-    Map<std::string, unsigned> wins = Map<std::string, unsigned>();
-    wins.add("team 1", 6);
-    wins.add("team 2", 7);
-    wins.add("team 3", 8);
+#include "CommandLineInterpreter.h"
 
-    for(const auto &[fst, snd] : wins) {
-        std::cout << fst << " " << snd << std::endl;
+#include <sstream>
+
+int main()
+{
+    ChampionshipHistory history;
+    Championship championship = Championship(TeamManager());
+    FootballGameSimulator simulator;
+
+    CommandLineInterpreter cli(history, championship, simulator);
+
+    cli.execute(Command::MENU, {});
+
+    std::string line;
+
+    while (true)
+    {
+        std::cout << "> ";
+
+        if (!std::getline(std::cin, line))
+            break;
+
+        if (line.empty())
+            continue;
+
+        std::stringstream ss(line);
+
+        std::string commandValue;
+        ss >> commandValue;
+
+        if (commandValue == "exit")
+            break;
+
+        Command cmd = parseCommand(commandValue);
+
+        std::vector<std::string> args;
+        std::string arg;
+
+        while (ss >> arg)
+        {
+            args.push_back(arg);
+        }
+
+        try
+        {
+            cli.execute(cmd, args);
+        }
+        catch (const std::exception& e)
+        {
+            std::cerr << "Command Error: "
+                      << e.what()
+                      << std::endl;
+        }
+        catch (...)
+        {
+            std::cerr << "Unknown command error occurred!"
+                      << std::endl;
+        }
     }
 
     return 0;
